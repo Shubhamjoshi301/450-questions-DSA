fad1526436fbbb51c25807797367cf1b8046ac14
Shubham Joshi <joshishubham0301@gmail.com>
2022-05-24 10:19:26 +0530

    ✅stacks and queues circular tour

---
+++ stacks and queues/circular_tour.py
@@ -0,0 +1,85 @@
+'''
+Suppose there is a circle. There are N petrol pumps on that circle. You will be given two sets of data.
+1. The amount of petrol that every petrol pump has.
+2. Distance from that petrol pump to the next petrol pump.
+Find a starting point where the truck can start to get through the complete circle without exhausting its petrol in between.
+Note :  Assume for 1 litre petrol, the truck can go 1 unit of distance.
+
+Example 1:
+
+Input:
+N = 4
+Petrol = 4 6 7 4
+Distance = 6 5 3 5
+Output: 1
+Explanation: There are 4 petrol pumps with
+amount of petrol and distance to next
+petrol pump value pairs as {4, 6}, {6, 5},
+{7, 3} and {4, 5}. The first point from
+where truck can make a circular tour is
+2nd petrol pump. Output in this case is 1
+(index of 2nd petrol pump).
+
+Your Task:
+Your task is to complete the function tour() which takes the required data as inputs and returns an integer denoting a point from where a truck will be able to complete the circle (The truck will stop at each petrol pump and it has infinite capacity). If there exists multiple such starting points, then the function must return the first one out of those. (return -1 otherwise)
+
+Expected Time Complexity: O(N)
+Expected Auxiliary Space : O(1)
+
+Constraints:
+2 ≤ N ≤ 10000
+1 ≤ petrol, distance ≤ 1000
+
+'''
+
+'''
+    lis[][0]:Petrol
+    lis[][1]:Distance
+'''
+
+class Solution:
+    
+    #Function to find starting point where the truck can start to get through
+    #the complete circle without exhausting its petrol in between.
+    def tour(self,lis, n):
+        #Code here
+        start = 0
+        petrol = 0
+        dist = 0
+        
+        for i in range(n):
+            petrol += lis[i][0]
+            dist += lis[i][1]
+            # print(petrol,dist)
+        if petrol < dist :
+                return -1
+            
+        s = 0
+        d = 0
+        for i in range(n):
+            s += lis[i][0] - lis[i][1]
+            
+            
+            if s < 0:
+                start = i + 1
+                d += s
+                s = 0
+            
+            
+        
+                
+        return start 
+
+#{ 
+#  Driver Code Starts
+if __name__ == '__main__': 
+    t = int(input())
+    for i in range(t):
+        n = int(input())
+        arr=list(map(int, input().strip().split()))
+        lis=[]
+        for i in range(1, 2*n, 2):
+            lis.append([ arr[i-1], arr[i] ])
+        print(Solution().tour(lis, n))
+    # Contributed by: Harshit Sidhwa
+# } Driver Code Ends
