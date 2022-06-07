4dfa340f3eb240d2a0fa9e17c784448a5084eb26
Shubham Joshi <joshishubham0301@gmail.com>
2022-06-07 14:10:07 +0530

    binary right view

---
+++ binary tree/right_view.py
@@ -0,0 +1,165 @@
+'''
+Given a Binary Tree, find Right view of it. Right view of a Binary Tree is set of nodes visible when tree is viewed from right side.
+
+Right view of following tree is 1 3 7 8.
+
+          1
+       /     \
+     2        3
+   /   \      /    \
+  4     5   6    7
+    \
+     8
+
+Example 1:
+
+Input:
+       1
+    /    \
+   3      2
+Output: 1 2
+Example 2:
+
+Input:
+     10
+    /   \
+  20     30
+ /   \
+40  60 
+Output: 10 30 60
+Your Task:
+Just complete the function rightView() that takes node as parameter and returns the right view as a list. 
+
+Expected Time Complexity: O(N).
+Expected Auxiliary Space: O(Height of the Tree).
+
+Constraints:
+1 ≤ Number of nodes ≤ 105
+1 ≤ Data of a node ≤ 105
+'''
+
+# Code:
+
+#User function Template for python3
+
+
+
+'''
+# Node Class:
+class Node:
+    def init(self,val):
+        self.data = val
+        self.left = None
+        self.right = None
+'''
+class Solution:
+    #Function to return list containing elements of right view of binary tree.
+    def rightView(self,root):
+        if root == None:
+            return []
+        # code here
+        q = []
+        ans = []
+        q.append(root)
+        while(q):
+            n = len(q)
+            
+            
+            for i in range(n):
+                temp = q.pop(0)
+                if i == 0:
+                    ans.append(temp.data)
+                if temp.right:
+                    q.append(temp.right)
+                if temp.left:
+                    q.append(temp.left)
+        return ans    
+#{ 
+#  Driver Code Starts
+#Initial Template for Python 3
+
+
+#Initial Template for Python 3
+
+#Contributed by Sudarshan Sharma
+from collections import deque
+import queue
+# Tree Node
+class Node:
+    def __init__(self, val):
+        self.right = None
+        self.data = val
+        self.left = None
+
+    
+# Function to Build Tree   
+def buildTree(s):
+    #Corner Case
+    if(len(s)==0 or s[0]=="N"):           
+        return None
+        
+    # Creating list of strings from input 
+    # string after spliting by space
+    ip=list(map(str,s.split()))
+    
+    # Create the root of the tree
+    root=Node(int(ip[0]))                     
+    size=0
+    q=deque()
+    
+    # Push the root to the queue
+    q.append(root)                            
+    size=size+1 
+    
+    # Starting from the second element
+    i=1                                       
+    while(size>0 and i<len(ip)):
+        # Get and remove the front of the queue
+        currNode=q[0]
+        q.popleft()
+        size=size-1
+        
+        # Get the current node's value from the string
+        currVal=ip[i]
+        
+        # If the left child is not null
+        if(currVal!="N"):
+            
+            # Create the left child for the current node
+            currNode.left=Node(int(currVal))
+            
+            # Push it to the queue
+            q.append(currNode.left)
+            size=size+1
+        # For the right child
+        i=i+1
+        if(i>=len(ip)):
+            break
+        currVal=ip[i]
+        
+        # If the right child is not null
+        if(currVal!="N"):
+            
+            # Create the right child for the current node
+            currNode.right=Node(int(currVal))
+            
+            # Push it to the queue
+            q.append(currNode.right)
+            size=size+1
+        i=i+1
+    return root
+    
+    
+if __name__=="__main__":
+    t=int(input())
+    for _ in range(0,t):
+        s=input()
+        root=buildTree(s)
+        result = Solution().rightView(root)
+        for value in result:
+            print(value, end = " ")
+        print()
+        
+        
+
+# } Driver Code Ends
